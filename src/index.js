1275a0eca497a617c8276f04e5d5743e3a7b6cd1
zhulym <zhulym@mail.ru>
2021-07-04 13:56:44 +0300

    fix: fix some problems

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 module.exports = function toReadable(n) {
-    const arrNum1 = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+    const arrNum1 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const arrNum2 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     const tensHundreds = +(n.toString().slice(0, 1));
     const once = +(n.toString().slice(-1));
     const numTo120 = +(n.toString().slice(-2));
     const hundredsTens = +(n.toString().slice(1, 2));
 
+    if (n === 0) return 'zero';
+
     if (n > 0 && n < 20) {
-        return arrNum1[n];
+        return arrNum1[n].trim();
     }
 
     if (n >= 20 && n < 100) {
@@ -15,10 +17,10 @@ module.exports = function toReadable(n) {
     }
 
     if ((n - (tensHundreds * 100)) < 20 && n >= 100) {
-        return `${arrNum1[tensHundreds]}hundred ${arrNum1[numTo120]}`
+        return `${arrNum1[tensHundreds]} hundred ${arrNum1[numTo120]}`
     }
 
     if (n >= 120 && n < 1000) {
-        return `${arrNum1[tensHundreds]}hundred ${arrNum2[hundredsTens]} ${arrNum1[once]}`
+        return `${arrNum1[tensHundreds]} hundred ${arrNum2[hundredsTens]} ${arrNum1[once]}`
     }
 }
