6ef64e7ed33092ac574bbb09cd09b87e848b3256
zhulym <zhulym@mail.ru>
2021-07-04 13:26:10 +0300

    feat: implement human readeble function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(n) {
+    const arrNum1 = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+    const arrNum2 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const tensHundreds = +(n.toString().slice(0, 1));
+    const once = +(n.toString().slice(-1));
+    const numTo120 = +(n.toString().slice(-2));
+    const hundredsTens = +(n.toString().slice(1, 2));
+
+    if (n > 0 && n < 20) {
+        return arrNum1[n];
+    }
+
+    if (n >= 20 && n < 100) {
+        return `${arrNum2[tensHundreds]} ${arrNum1[once]}`
+    }
+
+    if ((n - (tensHundreds * 100)) < 20 && n >= 100) {
+        return `${arrNum1[tensHundreds]}hundred ${arrNum1[numTo120]}`
+    }
+
+    if (n >= 120 && n < 1000) {
+        return `${arrNum1[tensHundreds]}hundred ${arrNum2[hundredsTens]} ${arrNum1[once]}`
+    }
 }
